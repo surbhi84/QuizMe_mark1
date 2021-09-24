8bddaf0c5692415dae81efe69f701050d1888b92
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2021-09-25 02:49:06 +0530

    feat: name() defined

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 var readlineSync = require("readline-sync");
 var chalk = require("chalk");
 var score = 0;
+
+function name() {
+  var userName = readlineSync.question(chalk.blue("Please enter your name.."));
+  return userName;
+}
+
+console.log(chalk.yellow.bold("Welcome to a MINI QUIZ about ME!"));
